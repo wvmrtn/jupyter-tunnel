25eeedf082c9193de9304211768dbea608a6c07a
wvmrtn <william.vincent.martin@gmail.com>
2020-10-19 13:24:52 +0200

    fixed argument parsing

--- jupyter_tunnel.sh
+++ jupyter_tunnel.sh
@@ -3,9 +3,9 @@
 # Custom functions
 # Fires-up a Jupyter notebook by supplying a specific port
 function jpt(){
-    jupyter notebook --no-browser --port=$1
+    jupyter notebook --no-browser --port="$1"
 }
 # Forwards port $1 into port $2 and listens to it from remoteuser@remotehost $3
 function jptt(){
-    ssh -N -f -L localhost:$2:localhost:$1 $3
+    ssh -N -f -L localhost:"$2":localhost:"$1" "$3"
 }
