10217103f2febeeb4ca0a79e8530c143ed94ea07
wvmrtn <william.vincent.martin@gmail.com>
2020-10-19 11:58:03 +0200

    added first functions to jupyter tunnel

---
+++ jupyter_tunnel.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Custom functions
+# Fires-up a Jupyter notebook by supplying a specific port
+function jpt(){
+    jupyter notebook --no-browser --port=$1
+}
+# Forwards port $1 into port $2 and listens to it from remoteuser@remotehost $3
+function jptt(){
+    ssh -N -f -L localhost:$2:localhost:$1 $3
+}
